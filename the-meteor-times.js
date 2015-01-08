6764f772fa792e7f5f462394842416bee91612a1
Leons Petrazickis <leonsp@ca.ibm.com>
2015-01-07 19:27:58 -0500

    Ugliest form ever

--- the-meteor-times.js
+++ the-meteor-times.js
@@ -1,1 +1,41 @@
 Articles = new Mongo.Collection("articles");
+
+if (Meteor.isClient) {
+  console.log("Client!");
+  Template.body.events({
+    "submit .article-form": function(event) {
+      console.log("Form submitted!");
+      var headline = event.target.headline.value;
+      var text = event.target.text.value;
+
+      event.target.headline.value = "";
+      event.target.text.value = "";
+
+      Meteor.call("addArticle", headline, text);
+
+      return false;
+    },
+    "click .delete": function () {
+      Meteor.call("deleteArticle", this._id);
+    }
+  })
+}
+
+Meteor.methods({
+  addArticle: function(headline, text) {
+    console.log("Adding on server!");
+    if (!Meteor.userId()) {
+      throw new Meteor.Error("Not authorized");
+    }
+    var author = Meteor.user().username;
+
+    Articles.insert({
+      headline: headline,
+      text: text,
+      author: { name: author }
+    });
+  },
+  deleteArticle: function(articleId) {
+    Articles.remove(articleId);
+  }
+})
