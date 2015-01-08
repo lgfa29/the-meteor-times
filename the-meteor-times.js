593c598c87cfe7ada2f59e8f40271b4a0c3d99ce
Luiz Aoqui <lgfa29@gmail.com>
2015-01-07 19:38:46 -0500

    add router

--- the-meteor-times.js
+++ the-meteor-times.js
@@ -1,1 +1,18 @@
 Articles = new Mongo.Collection("articles");
+
+Router.configure({
+  layoutTemplate: 'main'
+});
+
+Router.route("/", function() {
+  this.render("home");
+});
+
+Router.route("/articles/:id", function() {
+  this.render("article", {
+    data: function() {
+      return Articles.findOne({ _id: this.params.id });
+    },
+    name: 'article.show'
+  });
+});
